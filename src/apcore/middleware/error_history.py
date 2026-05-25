43ce7b4268f26c8b772b431705e19bddcfad32d3
tercel <tercel.yi@gmail.com>
2026-05-25 15:10:44 +0800

    chore: remove stale error_history deprecation shim (audit D9-004)
    Signed-off-by: tercel <tercel.yi@gmail.com>

--- src/apcore/middleware/error_history.py
+++
@@ -1,9 +0,0 @@
-"""Backward-compatibility shim. Import ErrorHistoryMiddleware from error_history_middleware instead."""
-
-import warnings
-
-warnings.warn(
-    "apcore.middleware.error_history is deprecated; use apcore.middleware.error_history_middleware",
-    DeprecationWarning,
-    stacklevel=2,
-)
